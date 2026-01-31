6a36302c7f047561c0ef4dce981c322fc8b99a84
tanmay raj <tanmayrajdec31@gmail.com>
2026-02-01 00:41:37 +0530

    enable cors

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,14 @@ import dotenv from 'dotenv';
 import todoRoutes from './routes/todoRoute.js'
 import { connectDB } from './config/db.js';
 import bodyParser from 'body-parser';
+import cors from "cors";
+
 dotenv.config();
 
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/api/todos", todoRoutes)
 
